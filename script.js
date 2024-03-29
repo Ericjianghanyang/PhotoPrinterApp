0da3b73b900311533380f0e837051bb6488546fd
Ericjianghanyang <162102006+Ericjianghanyang@users.noreply.github.com>
2024-03-29 11:59:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+document.getElementById('image-input').addEventListener('change', function() {
+    var fileInput = this;
+    var file = fileInput.files[0];
+
+    // 检查是否选择了图片
+    if (file) {
+        // 显示打印按钮
+        document.getElementById('print-button').style.display = 'block';
+
+        // 预览图片
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('image-preview').src = e.target.result;
+            document.getElementById('image-preview').style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    } else {
+        // 如果没有选择图片，则隐藏按钮和预览图片
+        document.getElementById('print-button').style.display = 'none';
+        document.getElementById('image-preview').style.display = 'none';
+    }
+});
+
 document.getElementById('print-button').addEventListener('click', function() {
     var fileInput = document.getElementById('image-input');
     var file = fileInput.files[0];
